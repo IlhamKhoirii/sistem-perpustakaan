aedb350ec9203db44cf364922d910e94479b763c
Ilham Khoiri <230103063@mhs.udb.ac.id>
2025-08-04 12:22:35 +0700

    memperbaiki routes peminjaman

--- backend/app/routes/routes_peminjaman.py
+++ backend/app/routes/routes_peminjaman.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from datetime import date
+from datetime import date, timedelta
 from typing import List
 
 from app.database import get_db
